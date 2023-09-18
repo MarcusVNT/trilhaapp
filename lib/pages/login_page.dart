8d611c46eed97d3b132d86fb0d93a45718ba873f
MarcusVNT <marcus-vnt@live.com>
2023-09-18 15:39:18 -0300

    Add Logo on login screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -21,12 +22,36 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(
                 height: 80,
               ),
-              Container(
-                  width: 150,
-                  height: 150,
-                  color: const Color.fromARGB(255, 192, 177, 177),
-                  child:
-                      const Icon(Icons.person, size: 100, color: Colors.white)),
+              Row(
+                children: [
+                  Expanded(child: Container()),
+                  Expanded(
+                    flex: 3,
+                    child: Image.network(
+                      "https://yt3.ggpht.com/ytc/AMLnZu-zyneRCxau44njO1mUk0e8h_0GkHoA5v4j3-h6CQ=s900-c-k-c0x00ffffff-no-rj",
+                      height: 200,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Já possui uma conta?",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Faça login para controlar sua casa sobre rodas!",
+                style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
+              ),
               const SizedBox(
                 height: 40,
               ),
@@ -36,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                 margin: const EdgeInsets.symmetric(horizontal: 30),
                 color: const Color.fromARGB(255, 115, 123, 173),
                 height: 30,
-                child: const Text("Informe seu nickname:"),
+                child: const Text("Informe Seu Nickname:"),
               ),
               const SizedBox(
                 height: 5,
@@ -59,7 +84,7 @@ class _LoginPageState extends State<LoginPage> {
                 margin: const EdgeInsets.symmetric(horizontal: 30),
                 color: const Color.fromARGB(255, 115, 123, 173),
                 height: 30,
-                child: const Text("Informe seu passwaord:"),
+                child: const Text("Informe Seu Passwaord:"),
               ),
               const SizedBox(
                 height: 5,
@@ -74,28 +99,41 @@ class _LoginPageState extends State<LoginPage> {
                 child: const Text("Password:"),
               ),
               const SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: Container(),
+                height: 30,
               ),
               Container(
                 alignment: Alignment.center,
                 width: double.infinity,
                 margin: const EdgeInsets.symmetric(horizontal: 30),
-                color: const Color.fromARGB(255, 115, 123, 173),
+                color: const Color.fromARGB(255, 73, 98, 255),
                 height: 30,
                 child: const Text("Login"),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                child: const Text(
+                  "Esqueci Minha Senha",
+                  style: TextStyle(fontWeight: FontWeight.w400),
+                ),
               ),
               const SizedBox(
                 height: 20,
               ),
               Container(
-                alignment: Alignment.center,
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                height: 30,
-                child: const Text("Sign Up"),
-              ),
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  height: 30,
+                  child: const Text("Criar Conta",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 73, 98, 255),
+                          fontWeight: FontWeight.w600))),
               const SizedBox(
                 height: 40,
               ),
